aad5a2eced4c64c9033e27848517d35ae15d0e57
mousonmars@googlemail.com <mousonmars@googlemail.com>
2024-02-07 21:53:10 +0100

    pandas

--- dataframes.py
+++ dataframes.py
@@ -2,10 +2,11 @@ import warnings
 warnings.filterwarnings("ignore", "\nPyarrow", DeprecationWarning)
 import pandas as pd
 
-df = pd.read_csv(r"E:\Lerning\Learning\test4.csv", encoding='utf-8')
+df = pd.read_csv(r"E:\Lerning\Learning\test4.csv", encoding='utf-8', sep=';')
+# mit sep kann man das Trennzeichen der csv definieren !
 print(df)
 print(df.iloc[0,0])
 print(df.loc[0,"Nachname"])
 print(df.loc[1,"Name"])
 
-print(df.loc[1:3,"Name"])
+print(df.iloc[1])
