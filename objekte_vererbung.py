8718d15b34e9d13de2df77d905aa7e1e486cec4b
mousonmars@googlemail.com <mousonmars@googlemail.com>
2024-02-07 11:53:59 +0100

    funktionen und import test

---
+++ objekte_vererbung.py
@@ -0,0 +1,55 @@
+from datetime import date
+
+current_year = date.today()
+current_year = int(current_year.strftime("%Y"))
+
+class person:
+    # Blaupause für objekt, hier werden alle Parameter als Platzhalter eingegeben
+    # Self definiert die variable innerhalb der Klasse
+    # Übergabevariablen werden in der init klammer definiert
+    def __init__(self, name, yob, color, gender, height):
+        self.yob = yob
+        self.color = color
+        self.gender = gender
+        self.name = name
+        self.klasse = 1
+        self.height = height
+
+
+    # Methode die ein Obejekt ausführen kann  (im Prinzip eine funktion der class)
+    def calc_age(self):
+        age = current_year - self.yob
+        return age
+
+    def rollercoaster(self):
+        if self.height > 100 and self.height < 150:
+            return True
+        elif self.height > 150:
+            return "Zu Groß"
+        else:
+            return False
+        #mischen von print und anderen ausgaben ist doof
+
+
+class male(person):
+    def __init__(self, name, yob, color, gender, height, work):
+        self.work = work
+        super().__init__(name, yob, color, gender, height)
+
+
+
+
+Tobi = person("tobi", 1980, "blue", "male", 178)
+Anna = person("Anna", 1986, "blue", "female", 160)
+Emma = person("Emma", 2016, "blue", "female", 90)
+
+Lara =male("lara", 2018, "female","female", 110, "Kindergarten")
+print(Lara.work)
+
+
+
+print(Emma.rollercoaster())
+print(Tobi.rollercoaster())
+
+
+
